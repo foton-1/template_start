7ebf51f70d7adbf6786a8e06f461c37fd380d8cd
foton-1 <ovf0675@outlook.com>
2023-05-20 19:12:17 +0300

    2023.05.20
    створено

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-// --подія input змінює інлайн-стиль font-size--
-const sizeInput = document.querySelector('#font-size-control');
-const textEl = document.querySelector('#text');
-
-sizeInput.addEventListener('input', () => {
-  textEl.style.fontSize = `${sizeInput.value}px`;
-});
